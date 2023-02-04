12f9d8835ee4c43e9f6be9996c64ea33f30f1e34
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-02-04 21:11:58 +0500

    added the notifications sdk settings

--- src/index.js
+++ src/index.js
@@ -20,6 +20,15 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 
+import admin from "firebase-admin";
+import serviceAccount from "./Muhammad_Talha/findmykids-c93cf-firebase-adminsdk-mh10z-914e5a4aec.json" assert { type: "json" };
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 const app = express();
 
 app.use(express.json());
@@ -38,11 +47,11 @@ app.get("/users/child/notifications",(req,res)=>{
 })
 
 app.post("/users/child/notifications",(req,res)=>{
-  const {data} = req.body;
-  const {notification,topic} = data;
+  // const {data} = req.body;
+  // const {notification,topic} = data;
   admin.messaging().send({
     notification:{
-      ...notification
+     title:""
     },
     topic:topic,
     // token:"dLEQ0VuLSy-wZyE7fbgdgf:APA91bHnVei6Hv_eNMnLElORqLEVWFjD9g-k-wChUzGiSxMNak48lRf3ViM5hIFheH_u7m6LcYkpg60hCbYY7d5JLUQOKkCGGmhD3zAi2gMYpuzSHxcnh-oC5f1ZYBI5D2kPVncMVUBc",
