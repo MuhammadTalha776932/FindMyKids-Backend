62771b4009ecb3c107b98290d7668ae0de380150
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-02-04 20:50:37 +0500

    added the notifications route

--- src/index.js
+++ src/index.js
@@ -32,6 +32,24 @@ app.get("/",(req,res)=>{
   })
 })
 
+
+app.get("/users/child/notifications",(req,res)=>{
+  res.status(200);
+})
+
+app.post("/users/child/notifications",(req,res)=>{
+  const {data} = req.body;
+  const {notification,topic} = data;
+  admin.messaging().send({
+    notification:{
+      ...notification
+    },
+    topic:topic,
+    // token:"dLEQ0VuLSy-wZyE7fbgdgf:APA91bHnVei6Hv_eNMnLElORqLEVWFjD9g-k-wChUzGiSxMNak48lRf3ViM5hIFheH_u7m6LcYkpg60hCbYY7d5JLUQOKkCGGmhD3zAi2gMYpuzSHxcnh-oC5f1ZYBI5D2kPVncMVUBc",
+  })
+})
+
+
 app.get("/users", async (req, res) => {
   const users = [];
   const colRef = collection(db, "users");
