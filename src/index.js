356d854cfb8a223b2269ef39a06c63d4363ed777
Muhammad-Talha <122554285+MuhammadTalha776932@users.noreply.github.com>
2023-02-05 15:02:42 +0500

    Update the index.js

--- src/index.js
+++ src/index.js
@@ -48,10 +48,9 @@ app.get("/users/child/notifications",(req,res)=>{
 
 app.post("/users/child/notifications",(req,res)=>{
   const {data} = req.body;
-  const {notification,topic} = data?.notificationMessage;
+  const notificationMessage = data?.notificationMessage;
   admin.messaging().send({
-    notifications,
-    topic
+    ...notificationMessage
     // token:"dLEQ0VuLSy-wZyE7fbgdgf:APA91bHnVei6Hv_eNMnLElORqLEVWFjD9g-k-wChUzGiSxMNak48lRf3ViM5hIFheH_u7m6LcYkpg60hCbYY7d5JLUQOKkCGGmhD3zAi2gMYpuzSHxcnh-oC5f1ZYBI5D2kPVncMVUBc",
   })
 })
