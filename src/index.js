035971d810bd95c36971e74a31062cca0c1150e4
Muhammad-Talha <122554285+MuhammadTalha776932@users.noreply.github.com>
2023-02-05 15:12:43 +0500

    again update the index notification route

--- src/index.js
+++ src/index.js
@@ -50,7 +50,8 @@ app.post("/users/child/notifications",(req,res)=>{
   const {data} = req.body;
   const notificationMessage = data?.notificationMessage;
   admin.messaging().send({
-    ...notificationMessage
+    notification: {...notificationMessage.notification},
+    topic: notificationMessage.topic
     // token:"dLEQ0VuLSy-wZyE7fbgdgf:APA91bHnVei6Hv_eNMnLElORqLEVWFjD9g-k-wChUzGiSxMNak48lRf3ViM5hIFheH_u7m6LcYkpg60hCbYY7d5JLUQOKkCGGmhD3zAi2gMYpuzSHxcnh-oC5f1ZYBI5D2kPVncMVUBc",
   })
   res.send({status:200,message:"OK"});
