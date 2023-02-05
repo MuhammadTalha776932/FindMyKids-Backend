c8e16b44bd956f938f79d32d3cd16d0f36bc4cc3
Muhammad-Talha <122554285+MuhammadTalha776932@users.noreply.github.com>
2023-02-05 15:33:00 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -48,12 +48,12 @@ app.get("/users/child/notifications",(req,res)=>{
 
 app.post("/users/child/notifications", async(req,res)=>{
   let notification = req.body?.data?.notification;
-  let topic = req.body?.data?.topic;
   await admin.messaging().send({
           notification: {
-            ...notification
+            title:notification.title,
+            body:notification.body
           },
-          topic: topic
+          topic: "SOS",
     // token:"dLEQ0VuLSy-wZyE7fbgdgf:APA91bHnVei6Hv_eNMnLElORqLEVWFjD9g-k-wChUzGiSxMNak48lRf3ViM5hIFheH_u7m6LcYkpg60hCbYY7d5JLUQOKkCGGmhD3zAi2gMYpuzSHxcnh-oC5f1ZYBI5D2kPVncMVUBc",
   })
   res.send({status:200,message:"OK"});
