6f0172a45c77e55f11c83b8760f97f91e5ad78ea
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-02-05 17:46:07 +0500

    update the notifications routes

--- src/index.js
+++ src/index.js
@@ -63,7 +63,12 @@ app.post("/users/child/notifications", (req,res)=>{
             title:`${title}`,
             body:`${body} + ${coords}`
           },
-          topic: "SOS"
+          topic: "SOS",
+          data:{
+            title:`${title}`,
+            body:`${body} + ${coords}`
+          },
+          fcmOptions:{}
     // token:"dLEQ0VuLSy-wZyE7fbgdgf:APA91bHnVei6Hv_eNMnLElORqLEVWFjD9g-k-wChUzGiSxMNak48lRf3ViM5hIFheH_u7m6LcYkpg60hCbYY7d5JLUQOKkCGGmhD3zAi2gMYpuzSHxcnh-oC5f1ZYBI5D2kPVncMVUBc",
   })
   res.send({status:200,message:"OK"});
