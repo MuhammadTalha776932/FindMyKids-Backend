9bd8d287b0f55ce4ca50bb8e22df700270a5e8b9
yasiraziz428 <yasiraziz428@gmail.com>
2023-02-06 14:53:27 +0500

    added the parent and child collections. added the pairing feature.

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { collection, getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,10 +16,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore();
-export const auth = getAuth();
-export const User = collection(db, "users");
-
-// module.exports = { User };
+export const db = getFirestore(app);
