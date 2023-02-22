db5e31a4f1d34d9b880c999f89d82d4a369d8981
yasiraziz428 <yasiraziz428@gmail.com>
2023-02-22 16:08:19 +0500

    Validate Pairing Fixed (setTimeOut to be fixed)

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -53,16 +53,13 @@ export const handlePostUser = async (req, res) => {
         code: c_code,
         isPaired: true,
       };
-      setDoc(docRef, data).then((response) => {
-        if (response) {
-          validatePairing(code).then((response) => {
-            setTimeout(() => {
-              res.send(response);
-            }, 3000);
-          });
-        }
-      });
-      //change parent field isPaired to true.. Write code below
+      await setDoc(docRef, data);
+      const response = await validatePairing(c_code);
+
+      setTimeout(async () => {
+        // console.log("Response:", response);
+        await res.send(response);
+      }, 5000);
     } else {
       res.send({ status: 200, message: "Already exists" });
     }
