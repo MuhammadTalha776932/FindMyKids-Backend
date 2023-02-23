9c771f0f18adbe38f57c4bd67b9a72c22d383d2e
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-02-23 12:51:36 +0500

    resolved the error in locationController

--- src/controllers/LocationController.js
+++ src/controllers/LocationController.js
@@ -1,17 +1,17 @@
 import { db } from "../configs/firebase.config.js";
 import {
-    addDoc,
-    collection,
-    deleteDoc,
-    doc,
-    getDoc,
-    getDocs,
-    onSnapshot,
-    query,
-    setDoc,
-    updateDoc,
-    where,
-  } from "firebase/firestore";
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 
 export const handleGetLocation = async (req, res) => {
   res.send({ status: 200, message: "DONE" });
@@ -21,6 +21,7 @@ export const handlePostLocation = async (req, res) => {
   const id = req.body?.data?.user?.uid || "";
   const deviceID = req.body.deviceID;
 
+  let arr = [];
   console.log(id);
   console.log(deviceID);
   if (deviceID == "Parent") {
@@ -30,14 +31,20 @@ export const handlePostLocation = async (req, res) => {
     // if parent exists
     if (docSnap.exists()) {
       console.log("coordinate/ exist if");
-
-      getDoc(docRef).then((response) => {
-        const childRef = response?.data()?.code;
+      let response = await getDoc(docRef);
+      const childRef = await response?.data()?.code;
+      if (typeof childRef === "object") {
+        for (const codes of childRef) {
+          const colRef = doc(db, "childs", codes);
+          let response = await getDoc(colRef).then(documents => documents.data())
+          arr.push(response);
+        }
+      } else {
         const colRef = doc(db, "childs", childRef);
         getDoc(colRef).then((response) => {
           res.send(response.data());
         });
-      });
+      }
     }
   }
 };
