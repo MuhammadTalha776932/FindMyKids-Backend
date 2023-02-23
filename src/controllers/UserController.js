a352b0c35b3182c26e6c72c40567ff62ba537c95
yasiraziz428 <yasiraziz428@gmail.com>
2023-02-23 20:11:40 +0500

    Child Validation intergrated

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -59,41 +59,55 @@ export const handlePostUser = async (req, res) => {
         res.send({ status: 200, message: "OK", email: uEmail, code })
       );
     }
-    //SIGN UP: If the CHILD does NOT exist, add the CHILD info in the firebase and also update isPaired value of its parent to TRUE
   } else if (deviceID == "Child") {
     const c_code = req.body?.data?.code;
-    const name = req.body?.data?.childInfo?.childState?.name;
-    const age = req.body?.data?.childInfo?.childState?.age;
+    const name = req.body?.data?.childInfo?.childState?.name || "";
+    const age = req.body?.data?.childInfo?.childState?.age || 0;
     const docRef = doc(db, "childs", c_code);
     const childSnapDoc = await getDoc(docRef);
-    if (!childSnapDoc.exists()) {
-      const data = {
-        email: uEmail,
-        deviceID: deviceID,
-        uid: id,
-        code: c_code,
-        name: name,
-        age: age,
-        isPaired: true,
-      };
-      await setDoc(docRef, data);
-      const response = await validatePairing(c_code); // Makes Parent isPaired: TRUE and returns the updated Parent Data
-      await res.send(response);
+    const response = await validatePairing(c_code); // Checks if code exists in parent return true otherwise false.
+
+    /////////// If code exists in the parent/////////////
+    if (response) {
+      //SIGN UP: If the CHILD does NOT exist, add the CHILD info in the firebase
+      if (!childSnapDoc.exists()) {
+        const data = {
+          email: uEmail,
+          deviceID: deviceID,
+          uid: id,
+          code: c_code,
+          name: name,
+          age: age,
+          isPaired: true,
+        };
+        await setDoc(docRef, data);
+        res.send(data);
+      } else {
+        //SIGN IN: if CHILD do exists send back MSG: "Already exists" as well as its parents code, isPaired value
+        const docRef = doc(db, "childs", c_code);
+        // const q = query(colRef, where("uid", "==", id));
+        const response = await getDoc(docRef);
+        if (response.data().uid === id) {
+          // If the uid of child matches with the uid in the DB
+          res.send([
+            {
+              status: 200,
+              message: "Already exists",
+              deviceID: response.data().deviceID,
+              code: response.data().code,
+              isPaired: response.data().isPaired,
+              uid: response.data().uid,
+            },
+          ]);
+        } else {
+          // If the uid of child NOT matches with the uid in the DB
+          res.send({ Message: "A user has already logged in with this code" });
+        }
+        // const obj = response.docs[0];
+      }
     } else {
-      //SIGN IN: if CHILD do exists send back MSG: "Already exists" as well as its parents code, isPaired value
-      const colRef = collection(db, "parents");
-      const q = query(colRef, where("code", "array-contains", c_code));
-      const response = await getDocs(q);
-      const obj = response.docs[0];
-      res.send([
-        {
-          status: 200,
-          message: "Already exists",
-          deviceID: obj.data().deviceID,
-          code: obj.data().code,
-          isPaired: obj.data().isPaired,
-        },
-      ]);
+      /////////// If code NOT exists in the parent////////////
+      res.send({ message: "Invalid Code" });
     }
   }
 };
--- src/services/validatePairing.js
+++ src/services/validatePairing.js
@@ -1,35 +1,14 @@
 import { db } from "../configs/firebase.config.js";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-  setDoc,
-  doc,
-  getDoc,
-} from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
-// Making isPaired TRUE on both parent and child sides. ***When the child logs in, it means it has paired to parent.***
 export const validatePairing = async (code) => {
   const colRef = collection(db, "parents");
-  const q = query(colRef, where("code", "==", code));
-  let arr = [];
-
+  const q = query(colRef, where("code", "array-contains", code));
   const response = await getDocs(q);
-  // Getting Parent Data by querying parent's secret code
-  for (const e of response.docs) {
-    const id = e.data().uid;
-    const parentRef = doc(db, "parents", id);
-    const parentSnap = await getDoc(parentRef);
-    if (parentSnap.exists()) {
-      await setDoc(parentRef, {
-        ...parentSnap.data(),
-        isPaired: true,
-      });
-      //Getting Parent again after setting Parent Doc.
-      const parentSnap1 = await getDoc(parentRef);
-      arr.push(parentSnap1.data());
-    }
+
+  if (response.docs[0]?.exists()) {
+    return true; // if code exists in the parent, only then return true
+  } else {
+    return false;
   }
-  return arr;
 };
