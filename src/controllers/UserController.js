c36ed4f00b72a2bcb6339ee771daf1df837a1539
Muhammad-Talha <122554285+MuhammadTalha776932@users.noreply.github.com>
2023-03-04 17:15:13 +0000

    return the array of code

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -50,7 +50,7 @@ export const handlePostUser = async (req, res) => {
           const docRef = doc(db, "parents", id);
           const docSnap = await getDoc(docRef);
           const filterForCode = docSnap.data()?.code
-          res.send({code:filterForCode[0]});
+          res.send({code:filterForCode});
         }
       } else {
         //if not array
