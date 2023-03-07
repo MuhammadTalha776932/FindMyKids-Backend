d26564a0d27c4cc6f09badcb94a70f889dd3629e
Muhammad-Talha <122554285+MuhammadTalha776932@users.noreply.github.com>
2023-03-07 08:30:10 +0000

    Testing the validation method.

--- src/services/validatePairing.js
+++ src/services/validatePairing.js
@@ -6,6 +6,10 @@ export const validatePairing = async (code) => {
   const q = query(colRef, where("code", "array-contains", code));
   const response = await getDocs(q);
 
+  // Testing purpose... only
+
+  console.log("===>>>",response);
+
   if (response.docs[0]?.exists()) {
     return true; // if code exists in the parent, only then return true
   } else {
