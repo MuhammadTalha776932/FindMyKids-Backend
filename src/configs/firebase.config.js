4c20a0b459d25b8169240fb18783ebdb498c87c8
Muhammad-Talha <122554285+MuhammadTalha776932@users.noreply.github.com>
2023-03-21 13:33:36 +0000

    changed the firebase.config.js file

--- src/configs/firebase.config.js
+++ src/configs/firebase.config.js
@@ -1,21 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCRezSu2H7KZA4ST0WLrBek6of6BIis7XI",
-  authDomain: "findmykids-153c6.firebaseapp.com",
-  projectId: "findmykids-153c6",
-  storageBucket: "findmykids-153c6.appspot.com",
-  messagingSenderId: "642135074952",
-  appId: "1:642135074952:web:cbf3cb0553bba5f0bed73d",
+  apiKey: "AIzaSyAT71kBzRYNpcMdMOLNa4nPt_PXdwYYypo",
+  authDomain: "findmykids-pingo.firebaseapp.com",
+  projectId: "findmykids-pingo",
+  storageBucket: "findmykids-pingo.appspot.com",
+  messagingSenderId: "484053342214",
+  appId: "1:484053342214:web:e6805a43ab24fb5b8f0b87"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
