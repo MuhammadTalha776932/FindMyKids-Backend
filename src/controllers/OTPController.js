66612344e8ebbd1d5ba51538a569a8159bc8e9c2
Muhammad-Talha <122554285+MuhammadTalha776932@users.noreply.github.com>
2023-03-24 09:24:52 +0000

    added the code field in response of OTPPost

--- src/controllers/OTPController.js
+++ src/controllers/OTPController.js
@@ -9,9 +9,8 @@ export const handleOTPGet = (req,res) =>{
 export const handleOTPPost = async (req,res) => {
     const c_code = req.body?.data?.code;
     const response = await validatePairing(c_code);
-
     if (response) {
-        res.send({isCorrect:response})
+        res.send({isCorrect:response,code:c_code})
     }else{
         res.send({isCorrect:response})
     }
